2eccb31e98044b3c41c8bc89c8174fced50782eb
Edison Liao <wliao75@gatech.edu>
2023-05-14 21:43:21 +0800

    add more comments

--- igCrawler.py
+++ igCrawler.py
@@ -1,5 +1,6 @@
-#pip install selenium
-#pip install webdriver_manager
+# Install the required packages 
+# pip install selenium, webdriver_manager, pandas
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -13,21 +14,20 @@ import time
 import pandas as pd
 import re
 
-
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-# Specify the path of "chromedriver.exe"
-# Remember to download the chromedriver 
+# Download the chromedriver according to your Chrome version at https://chromedriver.chromium.org/downloads  
+# Specify the path to "chromedriver.exe" on your computer
+# REMEMBER TO CHANGE THIS
 PATH = "C:/Users/ediso/Downloads/chromedriver_win32/chromedriver.exe"
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-# driver = webdriver.Chrome
 
-# Go to the IG login page
+# Go to the ig login page
 driver.get("https://www.instagram.com/")
 
-# Get the places to enter username and password
+# Get the two fields for username and password
 username = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.NAME, "username"))
 )
@@ -35,6 +35,7 @@ password = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.NAME, "password"))
 )
 
+# Make sure the username and password fields are clean
 username.clear()
 password.clear()
 
@@ -53,24 +54,29 @@ time.sleep(3)
 driver.get("https://www.instagram.com/beyondtaiwan/")
 time.sleep(5)
 
-# Scroll down once. In this case, can get the data of the latest 33 posts
+# Scroll down once. In this case, we get the data of the latest 33 posts
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
 time.sleep(3)
 
-# Select all posts
+# Select all 33 posts
 posts_elements = driver.find_elements(By.CLASS_NAME, "_aagu")
 print("Number of posts get: ", len(posts_elements))
 
-# List storing all posts and their data
+# A list storing all posts and their data
 posts = []
+
+# A list storing all posts labels
 posts_labels = []
 
+# Regular posts: regular ig posts, have insight data for analysis
 regular_posts = 0
+
+# Non-regular posts: like reels and video, do not have insight data for analysis
 non_regular_posts = 0
 
+# Iterate through all posts and click on them
 for post_element in posts_elements:
     driver.execute_script("arguments[0].click();", post_element)
-    # post.click()
     time.sleep(2)
 
     try:
@@ -81,13 +87,14 @@ for post_element in posts_elements:
 
         time.sleep(2)
 
+        # Get web element blocks that hold the data
+        # If the post has "external link taps" data, it'll have one more "wbloks_1"
         insight_blocks = driver.find_elements(By.CLASS_NAME, "wbloks_1")
         num_insight_blocks = len(insight_blocks)
         print("-----------------------------------")
         print("number of insight blocks", len(insight_blocks))
-        # If has "external link taps" will have 1 more "wbloks_1"
 
-        # The parse structures of likes, comments, replies, and saves are the same
+        # The parsing structures of likes, comments, replies, and saves data are the same
         # Likes
         likes_block = insight_blocks[14]
         likes_span = likes_block.find_element(By.XPATH, ".//span")
@@ -116,14 +123,14 @@ for post_element in posts_elements:
         interations = interactions_spans[0].text
         print("Interactions:", interations)
 
-        # The parse structure of profile visits and external link taps are the same
+        # The parsing structures of profile visits and external link taps are the same
         # Profile visits
         profile_visits_block = insight_blocks[37]
         profile_visits_spans = profile_visits_block.find_elements(By.XPATH, ".//span")
         profile_visits = profile_visits_spans[1].text
         print("Profile Visits:", profile_visits)
 
-        # Omit External link taps since not every post has it
+        # Omit External link taps data since not every post has it
         # External link taps
         # external_link_taps_block = insight_blocks[38]
         # external_link_taps_spans = external_link_taps_block.find_elements(By.XPATH, ".//span")
@@ -139,7 +146,7 @@ for post_element in posts_elements:
         print("Accounts Reached:", account_reach)
         print("Account Reached Not Follow:", account_reach_not_follow)
 
-        # The parse structure of impressions, from home, from hashtags, from profile, from other, follows back are the same
+        # The parsing structures of impressions, from home, from hashtags, from profile, from other, follows back are the same
         # Impressions 
         impressions_block = insight_blocks[49] if num_insight_blocks == 55 else insight_blocks[48]
         impressions_spans = impressions_block.find_elements(By.XPATH, ".//span")
@@ -194,12 +201,15 @@ for post_element in posts_elements:
         follows = follows_block_spans[1].text
         print("Follows:", follows)
 
+        # Store all posts data
         post_data = [likes, comments, replies, saves, interations, profile_visits, account_reach, account_reach_not_follow,\
                 impressions, impressions_from_home, impressions_from_profile, impressions_from_hashtag, impressions_from_other, follows]
-        
+    
+        # Remove "," in the data strings
         post_data = [data.replace(",", "") for data in post_data]
         posts.append(post_data)
 
+        # Get post labels
         post_label_h1 = driver.find_element(By.XPATH, "//*[contains(text(), '【')]")
         post_label_text = post_label_h1.text
         post_label_text_lst = re.findall(r"【(.*?)】", post_label_text)
@@ -207,36 +217,43 @@ for post_element in posts_elements:
         posts_labels.append(post_label_text)
         print("Post Label:", post_label_text)        
 
-        # Click the Chrome's go back button to close the post
+        # Click Chrome's go back button to close the post
         time.sleep(3)
         driver.execute_script("window.history.go(-1)") 
         time.sleep(3)
 
+    # Execute when the post is not a regular post
     except NoSuchElementException:
         non_regular_posts += 1
         # Click the Chrome's go back button to close the post
         time.sleep(2)
         driver.execute_script("window.history.go(-1)") 
 
+# Display the number of posts get
 print("Number of posts get: ", len(posts_elements))
 print("Number of non-regular posts: ", non_regular_posts)
 print("Number of regular posts: ", regular_posts)
+
+# Create dataframe
 df = pd.DataFrame(posts, columns=["likes", "comments", "replies", "saves", "interactions", "profile_visits", "account_reach", "account_reach_not_follow"\
                                 , "impressions", "impressions_from_home", "impressions_from_hashtag", "impressions_from_profile", "impressions_from_other", "follows"])
 
+# For displaying the entire dataframe in terminal
 pd.set_option('display.max_columns', None)  # Show all columns
 pd.set_option('display.max_rows', None)  # Show all rows
 pd.set_option('display.width', None)  # Auto-expand the display
 pd.set_option('display.expand_frame_repr', False)  # Prevent the dataframe from wrapping
 
+# Convert data from str to int
 df = df[~df["impressions"].str.contains(r'[^0-9]')]
 for col in df.columns:
     df[col] = df[col].astype(int)
 
-# Insert post label column
+# Insert the post label column into the dataframe
 df.insert(loc = 0, column = "label", value = posts_labels)
 print(df)
 
+# Save data as an Excel file
 today = date.today()
 date_format = today.strftime("%b_%d")
-df.to_excel("ig_post_data" + date_format + ".xlsx", index=False)
+df.to_excel("ig_post_data_" + date_format + ".xlsx", index=False)
